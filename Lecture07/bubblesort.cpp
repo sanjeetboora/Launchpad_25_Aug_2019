eff5bf99911b42f12cda407261ac109be4d02a8c
sanjeetboora <sanjeet.boora97@gmail.com>
2019-10-17 17:18:17 +0530

    codes added

--- Lecture07/bubblesort.cpp
+++ Lecture07/bubblesort.cpp
@@ -1,6 +1,9 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
+bool compare(int a, int b){
+	return a<b;
+}
 
 void bubbleSort(int* arr, int n){
 
@@ -8,7 +11,8 @@ void bubbleSort(int* arr, int n){
 	{
 		for (int j = 0; j < n-1-i; j++)
 		{
-			if(arr[j]>arr[j+1]){
+			//if(arr[j]>arr[j+1]){
+			if(compare(arr[j],arr[j+1])){
 				int temp = arr[j];
 				arr[j] =arr[j+1];
 				arr[j+1] = temp;
